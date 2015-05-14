4995eaae65619a00dc403e55d2d9cc34d1b8d33d
Vivek Midididdi <vmidididdi@climate.com>
2015-05-14 15:30:47 +0000

    Modifying GDAL recipe to be up to date.

--- gdal/build.sh
+++ gdal/build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-UNAME=$(uname -s)
-if [ ${UNAME} = 'Darwin' ]; then
-    export CFLAGS="-mmacosx-version-min=`xcrun --sdk macosx --show-sdk-version`"
-    export CXXFLAGS="$CFLAGS"
-    export LDFLAGS="$CFLAGS"
-fi
-
-bash configure --with-python --prefix=$PREFIX --with-sqlite3=$PREFIX
-make
+# http://www.michael-joost.de/gdal_install.html
+unset CC CPP CXX
+CONFIG_SHELL=/bin/bash bash configure \
+    --with-python=$PREFIX/bin/python \
+    --with-hdf5=$PREFIX \
+    --with-netcdf=$PREFIX \
+    --prefix=$PREFIX
 make install
+
+ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
+DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
+mkdir -p $ACTIVATE_DIR
+mkdir -p $DEACTIVATE_DIR
+
+cp $RECIPE_DIR/posix/activate.sh $ACTIVATE_DIR/gdal-activate.sh
+cp $RECIPE_DIR/posix/deactivate.sh $DEACTIVATE_DIR/gdal-deactivate.sh
