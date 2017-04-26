dea8517e7f17ddefd440734e680c8b38a87f0c14
Yiqun Li <yiqun.li@climate.com>
2017-04-26 15:43:34 -0700

    do reverse 2dc9a229bae97628f7a9622ed06ae8ced937b8d5 and fix the conda build dependency issue

--- earthengine-api/build.sh
+++ earthengine-api/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# `setuptools` tries to fetch and install dependencies, causing conda build
-# to fail. Disable here and use conda for dependency management.
-# See: https://groups.google.com/a/continuum.io/forum/#!msg/conda/3Jw2cnsL_v8/IzxTSiQqomEJ
 $PYTHON setup.py install \
     --single-version-externally-managed \
-    --record="$RECIPE_DIR/record.txt"  # or /dev/null
+    --record="$RECIPE_DIR/record.txt"
