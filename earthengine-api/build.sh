410c89514d8f542460f2f00345acde4e5b4b3f03
Yiqun Li <yiqun.li@climate.com>
2017-04-26 13:49:05 -0700

    Disable setuptools dependency installation to fix build

--- earthengine-api/build.sh
+++ earthengine-api/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-$PYTHON setup.py install 
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
+# `setuptools` tries to fetch and install dependencies, causing conda build
+# to fail. Disable here and use conda for dependency management.
+# See: https://groups.google.com/a/continuum.io/forum/#!msg/conda/3Jw2cnsL_v8/IzxTSiQqomEJ
+$PYTHON setup.py install \
+    --single-version-externally-managed \
+    --record="$RECIPE_DIR/record.txt"  # or /dev/null
